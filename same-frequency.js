3a20754bb0b5314ca5d398582f6114eb546638c5
hectorchu~ <hector.chu828@gmail.com>
2022-01-14 17:22:06 -0800

    Added same-frequency function

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,32 @@
+"use strict"
 // add whatever parameters you deem necessary
-function sameFrequency() {
+
+/**
+ * takes in two numbers, returns whether each number's digit frequency is the same 
+ *
+ */
+function sameFrequency(num1, num2) {
+    const str1 = num1.toString();
+    const str2 = num2.toString();
+    const obj = {};
+
+    for (let i = 0; i < str1.length; i++) {
+        if (str1[i] in obj) {
+            obj[str1[i]]++;
+        } else {
+            obj[str1[i]] = 1;
+        }
+    }
+
+    for (let i = 0; i < str2.length; i++) {
+        if (str2[i] in obj) {
+            obj[str2[i]]--;
+            if (obj[str2[i]] === 0) {
+                delete obj[str2[i]];
+            }
+        } else {
+            return false;
+        }
+        console.log(obj);
+    }
 }
