a882e30944241c01134c59dc1fe128133c8e68b4
Kate Moser <komoser@gmail.com>
2022-01-14 18:05:20 -0600

    solved can-construct-word

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,30 @@
-// add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+"use strict";
+
+
+function canConstructWord(word, letters) {
+    let counter = makeCounter(letters);
+
+    for(let letter of word){
+        if(!counter[letter]){
+            return false;
+        }
+        counter[letter]--;
+        if(counter[letter] === 0){
+            delete counter[letter];
+        }
+        
+    }
+    return true;
+}
+
+/**Takes a string, creates and returns an object containing the counts for each character */
+function makeCounter(word){
+    let letterCounter = {};
+    for(let char of word){
+        let charCount = letterCounter[char] || 0;
+        letterCounter[char] = charCount + 1;
+    }
+    console.log(letterCounter);
+    return letterCounter;
+
 }
