10e3e32c9daf26aa83495f8252aecbcf2366ac68
hectorchu~ <hector.chu828@gmail.com>
2022-01-14 16:20:18 -0800

    Untested function

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,20 @@
+"use strict";
+
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(nums, targetAvg) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let sum = nums[left] + nums[right];
+        let avg = sum / 2;
+        if (avg === targetAvg) {
+            return true;
+        } else if (avg > targetAvg) {
+            right--;
+        } else {
+            left--;
+        }
+    }
+    return false;
 }
